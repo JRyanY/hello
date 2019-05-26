8bc7a080316df764a293fe09e990640e2a1b145c
codedrinker <616781846@qq.com>
2019-05-26 18:27:35 +0800

    aa

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,8 @@
+create TABLE user(
+    id int auto_increment primary key ,
+    account_id varchar(100),
+    name varchar(50),
+    token char(36),
+    gmt_create BIGINT,
+    gmt_modified bigint
+);

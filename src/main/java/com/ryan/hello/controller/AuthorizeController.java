2c31d8a34e335e4fe95ad85ad9c9bd3267e0d98c
codedrinker <616781846@qq.com>
2019-05-29 11:43:51 +0800

    aaaaaa

--- src/main/java/com/ryan/hello/controller/AuthorizeController.java
+++ src/main/java/com/ryan/hello/controller/AuthorizeController.java
@@ -49,7 +49,7 @@ public class AuthorizeController {
 
           String accessToken =  githubProvider.getAccessToken(new AccessTokenDTO());
           GithubUser githubUser = githubProvider.getUser(accessToken);
-          if(githubUser!=null){
+          if(githubUser!=null && githubUser.getId()!=null){
               User user = new User();
               String token = UUID.randomUUID().toString();
               user.setToken(token);
@@ -57,6 +57,7 @@ public class AuthorizeController {
               user.setAccountId(String.valueOf(githubUser.getId()));
               user.setGmtCreate(System.currentTimeMillis());
               user.setGmtModified(user.getGmtCreate());
+              user.setAvatarUrl(githubUser.getAvatar_url());
               userMapper.insert(user);
               response.addCookie(new Cookie("token",token));
               return "redirect:/";

536e40c5f0748a8c7204c9baa812059f3e78ac5c
codedrinker <616781846@qq.com>
2019-07-26 18:19:57 +0800

    修复数据库脚本

--- src/main/resources/db/migration/V8__Change_creator_column_type.sql
+++ src/main/resources/db/migration/V8__Change_creator_column_type.sql
@@ -1,2 +1,3 @@
-alter table question alter column creator bigint default not null;
-alter table comment alter column commentator bigint default not null;
+
+alter table question modify creator bigint  not null;
+alter table comment modify commentator bigint  not null;

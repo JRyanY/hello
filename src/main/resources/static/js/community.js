ed4cb8ecaccf6a76190b962c7a5147735e31f8a0
codedrinker <616781846@qq.com>
2019-07-28 15:32:48 +0800

    修改index、

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -138,3 +138,32 @@ function selectTag(e) {
     }
 
 }
+
+
+
+//time
+function showTime() {
+    var currentDate = new Date();
+    var startDate = new Date(2019, 2, 15);
+    var date3 = currentDate-startDate;
+    var days=Math.floor(date3/(24*3600*1000));
+    var leave1=date3%(24*3600*1000);     //计算天数后剩余的毫秒数
+    var hours=Math.floor(leave1/(3600*1000));
+    var leave2=leave1%(3600*1000);        //计算小时数后剩余的毫秒数
+    var minutes=Math.floor(leave2/(60*1000));
+    var leave3=leave2%(60*1000);          //计算分钟数后剩余的毫秒数
+    var seconds=Math.round(leave3/1000);
+    if (minutes < 10)
+        minutes = "0" + minutes;
+    if (seconds < 10)
+        seconds = "0" + seconds;
+    var currentTimeString = "Ryan and JY 在一起的 <br/>"
+        + "&nbsp&nbsp&nbsp&nbsp&nbsp"
+        + "<c style=\"color: deepskyblue;  font-family:Microsoft STHeiti; font-size:27px\" >" + days +" </c>day "
+        + "<c style=\"color: deepskyblue;  font-family:Microsoft STHeiti; font-size:27px\" >" + hours+" </c>hour "
+        +"<c style=\"color: deepskyblue;  font-family:Microsoft STHeiti; font-size:27px\" >" + minutes+" </c>min "
+        +"<c style=\"color: deepskyblue;  font-family:Microsoft STHeiti; font-size:27px\" >" +seconds+" </c>sec<br>"
+    document.getElementById("show").innerHTML=currentTimeString;
+    window.setTimeout("showTime()", 1000);
+}
+

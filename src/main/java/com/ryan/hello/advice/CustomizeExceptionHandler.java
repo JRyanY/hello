69ec92108fb5217edb2ae5335ee042ee1a7dc00b
codedrinker <616781846@qq.com>
2019-08-20 16:30:58 +0800

    a

--- src/main/java/com/ryan/hello/advice/CustomizeExceptionHandler.java
+++ src/main/java/com/ryan/hello/advice/CustomizeExceptionHandler.java
@@ -19,38 +19,38 @@ public class CustomizeExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     ModelAndView handle(Throwable e, Model model, HttpServletRequest request, HttpServletResponse response) {
-        String contentType=request.getContentType();
-        if("application/json".equals(contentType)){
+        String contentType = request.getContentType();
+        if ("application/json".equals(contentType)) {
             ResultDTO resultDTO;
             //返回JSON
             if (e instanceof CustomizeException) {
-                resultDTO= ResultDTO.errorOf((CustomizeException)e);
+                resultDTO = ResultDTO.errorOf((CustomizeException) e);
             } else {
-                resultDTO= ResultDTO.errorOf(CustomizeErrorCode.SYT_ERROR);
+                resultDTO = ResultDTO.errorOf(CustomizeErrorCode.SYT_ERROR);
             }
 
-            try{
+            try {
                 response.setContentType("application/json");
                 response.setStatus(200);
                 response.setCharacterEncoding("utf-8");
-                PrintWriter writer= response.getWriter();
-                 writer.write(JSON.toJSONString(resultDTO));
-                 writer.close();
-            }catch (IOException ioe){
+                PrintWriter writer = response.getWriter();
+                writer.write(JSON.toJSONString(resultDTO));
+                writer.close();
+            } catch (IOException ioe) {
 
             }
             return null;
 
-        }else{
-            //错误页面跳转
-        }
-        if (e instanceof CustomizeException) {
-               model.addAttribute("message",e.getMessage());
         } else {
-           model.addAttribute("message",CustomizeErrorCode.SYT_ERROR.getMessage());
+            //错误页面跳转
+
+            if (e instanceof CustomizeException) {
+                model.addAttribute("message", e.getMessage());
+            } else {
+                model.addAttribute("message", CustomizeErrorCode.SYT_ERROR.getMessage());
+            }
+            return new ModelAndView("error");
         }
-        return new ModelAndView("error");
     }
+
 }
-
-
